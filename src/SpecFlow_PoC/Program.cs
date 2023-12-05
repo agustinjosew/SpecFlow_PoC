a1e3713687e97ea4f2b707da5ae2554fc951adee
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-05 20:57:21 -0300

    creacion de esqueleto, estructura de PoC y agregado de SpecFlow

---
+++ src/SpecFlow_PoC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SpecFlow_PoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
