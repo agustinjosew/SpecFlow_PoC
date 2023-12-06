3e2c04a7c7f1ae85b7193fe3d2d4fe48f77df23d
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-05 22:12:24 -0300

    Implementacion de definicion de pasos para SpecFlow

--- src/SpecFlowProject_PoC.Specs/Features/Inventory.feature
+++ src/SpecFlowProject_PoC.Specs/Features/Inventory.feature
@@ -4,6 +4,16 @@ This feature is to test the inventory functionality
 
 @tag1
 Scenario: Get all inventory
-	Given There are 55 item in the inventory
-	When I get all the inventory
-	Then I should get 55 items
+ Given There are 55 items in the inventory
+ When I get all the inventory
+ Then I should get 55 items
+
+Scenario: Add item to inventory
+ Given There are 20 items in the inventory
+ When I add 5 item to inventory
+ Then I should get 25 items
+
+Scenario: Remove item From inventory
+ Given There are 100 items in the inventory
+ When I remove 20 item from inventory
+ Then I should get 80 items
--- src/SpecFlowProject_PoC.Specs/StepDefinitions/InventoryStepDefinitions.cs
+++ src/SpecFlowProject_PoC.Specs/StepDefinitions/InventoryStepDefinitions.cs
@@ -1,3 +1,4 @@
+using SpecFlow_PoC.Class;
 using System;
 using TechTalk.SpecFlow;
 
@@ -6,22 +7,41 @@ namespace SpecFlowProject_PoC.Specs.StepDefinitions
     [Binding]
     public class InventoryStepDefinitions
     {
-        [Given(@"There are (.*) item in the inventory")]
+        //Llamada al metodo de Inventario
+        Inventory _inventory = new Inventory();
+        private int _result;
+
+        [Given(@"There are (.*) items in the inventory")]
         public void GivenThereAreItemInTheInventory(int p0)
         {
-            throw new PendingStepException();
+            _inventory.Set(p0);
         }
 
         [When(@"I get all the inventory")]
         public void WhenIGetAllTheInventory()
         {
-            throw new PendingStepException();
+            _result = _inventory.GetQuantity();
         }
 
         [Then(@"I should get (.*) items")]
         public void ThenIShouldGetItems(int p0)
         {
-            throw new PendingStepException();
+            _result.Should().Be(p0);
         }
+
+        // Seccion de ADD ITEM
+        [When(@"I add (.*) item to inventory")]
+        public void WhenIAddItemToInventory(int p0)
+        {
+            _result = _inventory.Add(p0);
+        }
+
+        // Seccion de REMOVE ITEM
+        [When(@"I remove (.*) item from inventory")]
+        public void WhenIRemoveItemFromInventory(int p0)
+        {
+            _result = _inventory.Remove(p0);    
+        }
+
     }
 }
