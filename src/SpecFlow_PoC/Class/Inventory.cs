4c56e557bea87dd4f5b478ef75a343a4d7eb0ad6
Agustin Jose Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-05 21:35:15 -0300

    Developer features (#2)
    * creacion de esqueleto, estructura de PoC y agregado de SpecFlow
    
    * limpieza y preparacion de dependencias para SpecFlow
    
    * Updated .gitignore to exclude .vs directory
    
    * Agregado de exclusiones comunes para proyectos .NET y entradas especificas para SpecFlow que evita que se rastreen los archivos generados automaticamente durante la ejecucion de pruebas

---
+++ src/SpecFlow_PoC/Class/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlow_PoC.Class
+{
+    public class Inventory
+    {
+        public int CurrentQuantity { get; set; }
+
+        public int Set (int initialQuantity)
+        {
+            return CurrentQuantity = initialQuantity;
+        }
+
+        public int Add(int quantityToAdd)
+        {
+            CurrentQuantity += quantityToAdd;
+            return CurrentQuantity;
+        }
+
+        public int GetQuantity() 
+        { 
+            return CurrentQuantity;
+        }
+
+        public int Remove(int quantityToRemove)
+        {
+            CurrentQuantity -= quantityToRemove;
+            return CurrentQuantity;
+        }
+    }
+}
